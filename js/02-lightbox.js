6b04304145cc6fb3c2c17eed690259f1528149c1
AleksandraJarz <jarzebska.aleksandraa@gmail.com>
2023-12-03 22:47:03 +0100

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (image) => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+const galleryImages = document.querySelectorAll(".gallery__item");
+
+new SimpleLightbox(galleryImages, {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 console.log(galleryItems);
